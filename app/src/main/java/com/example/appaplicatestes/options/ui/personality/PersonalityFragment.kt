d5b9a6fc418b4856d3af8f84304c33de3ebed3da
FidelisSantos <vitorfidelissantos@gmail.com>
2022-12-02 09:53:52 -0300

    Subindo fluxo finalizado de personalidade

--- app/src/main/java/com/example/appaplicatestes/options/ui/personality/PersonalityFragment.kt
+++ app/src/main/java/com/example/appaplicatestes/options/ui/personality/PersonalityFragment.kt
@@ -8,7 +8,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.appaplicatestes.databinding.FragmentPersonalityBinding
 import com.example.appaplicatestes.options.ui.personality.activys.HtpActivity
+import com.example.appaplicatestes.options.ui.personality.activys.PalograficoActivity
 import com.example.appaplicatestes.options.ui.personality.activys.PfisterActivity
+import com.example.appaplicatestes.options.ui.personality.activys.QuatyActivity
 
 class PersonalityFragment : Fragment() {
 
@@ -29,6 +31,16 @@ class PersonalityFragment : Fragment() {
 
         binding.btnPFister.setOnClickListener {
             val intent = Intent(this.activity,PfisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnPalografico.setOnClickListener {
+            val intent = Intent(this.activity,PalograficoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnQuati.setOnClickListener {
+            val intent = Intent(this.activity,QuatyActivity::class.java)
             startActivity(intent)
         }
 
