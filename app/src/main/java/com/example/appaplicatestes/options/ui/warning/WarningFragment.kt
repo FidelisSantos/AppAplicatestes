7faa0381c65789d847778121946d9e7f97ee3b70
FidelisSantos <vitorfidelissantos@gmail.com>
2022-12-02 10:16:05 -0300

    Fechando metade do fluxo completo de atenção

--- app/src/main/java/com/example/appaplicatestes/options/ui/warning/WarningFragment.kt
+++ app/src/main/java/com/example/appaplicatestes/options/ui/warning/WarningFragment.kt
@@ -1,11 +1,14 @@
 package com.example.appaplicatestes.options.ui.warning
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.appaplicatestes.databinding.FragmentWarningBinding
+import com.example.appaplicatestes.options.ui.warning.activys.AcActivity
+import com.example.appaplicatestes.options.ui.warning.activys.BpaActivity
 
 
 class WarningFragment : Fragment() {
@@ -17,6 +20,18 @@ class WarningFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = FragmentWarningBinding.inflate(inflater,container, false)
+
+        binding.btnAc.setOnClickListener {
+            val intent = Intent(this.activity, AcActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnBpa.setOnClickListener {
+            val intent = Intent(this.activity, BpaActivity::class.java)
+            startActivity(intent)
+        }
+
+
         return binding.root
     }
 }
